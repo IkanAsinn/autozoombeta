ec6bbf977ab1e63afb794d80b2d7e8293d3ea379
IkanAsinn <qbin.hermawan@gmail.com>
2021-12-03 21:10:30 +0700

    Bugs Fix

--- autoZoom.py
+++ autoZoom.py
@@ -1,28 +1,43 @@
-import csv
+from csv import reader, writer
 import webbrowser
-import time
-import os
-import keyboard
+from time import strftime, sleep
+from os import system
+from datetime import timedelta
+
+def changeFormat(date: str):
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    day, month, year = date.split('/')
+    month = months[int(month) - 1]
+    return f'{day} {month} {year}'
 
 file = open('scheduled.csv', 'r')
-readed = csv.reader(file, delimiter=',')
+readed = reader(file, delimiter=',')
 scheduled = []
 for row in readed:
     if len(row) > 0:
         scheduled.append(row)
 
 file.close()
+
+today = strftime("%d/%m/%Y")
+today = changeFormat(today)
+
 isStarted = False
 for i in range(1, len(scheduled)):
     print("Next Schedule:", scheduled[i][0], "at", scheduled[i][1])
+    print("Class Name: {} - {}".format(scheduled[i][4], scheduled[i][5]))
     while True:
         if isStarted == False:
-            hour = int(time.strftime("%H"))
-            minute = int(time.strftime("%M"))
+            hour = int(strftime("%H"))
+            minute = int(strftime("%M"))
             start_hour = int(scheduled[i][1].split(':')[0])
             start_minute = int(scheduled[i][1].split(':')[1])
-            if hour == start_hour and minute == start_minute - 10: # 10 minutes before class
+            start_date = scheduled[i][0]
+            if hour >= start_hour and minute >= start_minute - 10 and today == start_date: # 10 minutes before class
                 isStarted = True
+
+            if hour >= 24:
+                today = changeFormat(strftime("%d/%m/%Y"))
 
         if isStarted == True:
             webbrowser.open(scheduled[i][8])
@@ -35,7 +50,24 @@ for i in range(1, len(scheduled)):
             start_hour *= 3600
             start_minute *= 60
             duration = ((end_hour + end_minute) - (start_hour + start_minute) - 300)
-            print('class started')
-            time.sleep(duration)
+            print('Class Started')
+            lines = list()
+            with open('scheduled.csv', 'r') as f:
+                reader = reader(f)
+                for row in reader:
+                    lines.append(row)
+
+            with open('scheduled.csv', 'w') as f:
+                writer = writer(f)
+                for row in lines:
+                    if len(row) > 0:
+                        if row[0] != scheduled[i][0] and row[1] != scheduled[i][1]:
+                            writer.writerow(row)
+
+            while duration > 0:
+                print('Time Left:', timedelta(seconds=duration))
+                sleep(1)
+                duration -= 1
+                system('cls')
             isStarted = False
             break
